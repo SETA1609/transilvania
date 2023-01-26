ebcbca78b554243f2804efd5aa930218724212f8
MarcRoggee <Marc.Roggee@brightstraining@.com>
2023-01-26 09:35:24 +0100

    Background Music und eatFoodSound lautstärke angepasst

--- src/main/java/com/brightstraining/javafxgame/Music.java
+++ src/main/java/com/brightstraining/javafxgame/Music.java
@@ -2,10 +2,19 @@ package com.brightstraining.javafxgame;
 
 import javafx.scene.media.AudioClip;
 
+import java.util.Objects;
+
 public class Music {
 
     public static void backGroundMusic(){
         AudioClip buzzer = new AudioClip(Music.class.getResource("/audio/Time.mp3").toExternalForm());
+        buzzer.setVolume(0.1);
+        buzzer.play();
+    }
+
+    public static void eatFoodSound(){
+        AudioClip buzzer = new AudioClip(Objects.requireNonNull(Music.class.getResource("/audio/gameboy.mp3")).toExternalForm());
+        buzzer.setVolume(0.1);
         buzzer.play();
     }
 }
